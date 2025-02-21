615f7cbb3fe71be466d760df53569da3fca77f70
khanzele16 <khanzele16@gmail.com>
2025-02-21 08:10:03 +0300

    added coursevideo

---
+++ src/pages/CourseVideos/CourseVideos.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CourseVideos() {
+  return (
+    <div className='flex flex-col gap-y-2'>
+        <h3 className='text-2xl'>Python разработчик</h3>
+    </div>
+  )
+}
+
+export default CourseVideos
